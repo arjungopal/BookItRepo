9ce8d1e2e047573d9dfd22cac746598762e00f31
arjungopal <arjun.g2@gmail.com>
2015-02-21 16:09:15 -0600

    Testing the db connection

---
+++ dbconnection/booksearch.php
@@ -0,0 +1,27 @@
+<?php
+   require_once 'dbConnect.php';
+	
+	$conn = dbConnect();	
+
+	$sql = "SELECT * from book ";
+	
+	$stmt = $conn->prepare($sql);
+	$results = $stmt->execute();
+	$rows = $stmt->fetchAll();		
+	
+	if(empty($rows)) {
+		echo "no records found";
+	}
+	else {		
+		
+		
+		foreach ($rows as $row) {
+			echo $row[1].'<br>';
+		}
+		
+	}
+		
+	
+?>
+
+
